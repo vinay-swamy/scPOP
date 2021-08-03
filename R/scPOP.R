6cea4d2fc24dcf2e5dac443a813664e775d78ec8
Swamy <swamyvinny@gmail.com>
2021-08-03 15:10:02 -0700

    properly generate all NAMESPACE lines with roxygen2. Add scPOP.R to import package dependencies

---
+++ R/scPOP.R
@@ -0,0 +1,10 @@
+#' scPOP: Metrics for Benchmarking scRNA-Seq Batch Correction
+#'
+#'  Evaluate using batch effect correction for scRNA-seq using multiple established methods, including the Adjusted Rand Index,
+#' Normalized Mutual Information, Local Inverse Simpson Index, and Silhouette Width. We also included metrics for #' aggregating and weighing multiple metrics together.
+#' @import Rcpp
+#' @docType package
+#' @name scPOP
+#' @useDynLib scPOP, .registration=TRUE
+NULL
+#> NULL
